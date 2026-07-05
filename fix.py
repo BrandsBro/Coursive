1d7d7bbb6eac50d11fad93ab07ed4942f30058f0
BrandsBro <mahtab.brandsbrollc@gmail.com>
2026-07-05 14:37:41 +0600

    description

--- fix.py
+++ fix.py
@@ -1,10 +1,2 @@
-content = open('components/admin/EmailTemplateEditor.jsx', encoding='utf-8').read()
-
-# Make left sidebar wider
-content = content.replace(
-    'gridTemplateColumns: "200px 1fr 300px"',
-    'gridTemplateColumns: "260px 1fr 320px"'
-)
-
-open('components/admin/EmailTemplateEditor.jsx', 'w', encoding='utf-8').write(content)
-print("Done!")
+content = open('app/api/stripe/create-account/route.js', encoding='utf-8').read()
+print(content[2000:4000])
