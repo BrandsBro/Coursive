7b6b1b3c7a3fda03419111659cd4adac11dfabb7
BrandsBro <mahtab.brandsbrollc@gmail.com>
2026-07-06 00:27:59 +0600

    message

--- fix.py
+++ fix.py
@@ -1,9 +1,2 @@
 content = open('components/quiz/PaymentModal.jsx', encoding='utf-8').read()
-
-content = content.replace(
-    '  displayPrice={displayPrice}/>',
-    '  displayPrice={activeDisplayPrice}/>'
-)
-
-open('components/quiz/PaymentModal.jsx', 'w', encoding='utf-8').write(content)
-print("Done!")
+print(content[3000:6000])
