ec4cf35e182399c5e500e814509f4d2c6ae840f1
Mabu-1 <umahtab65@gmail.com>
2026-07-08 21:29:39 +0600

    added

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -2,8 +2,9 @@ import Navbar from "@/components/layout/Navbar";
 export default function MainLayout({ children }) {
   return (
     <>
-      <Navbar />
+     
       <main style={{ maxWidth:1200, margin:"0 auto", padding:"16px 20px" }}>
+         <Navbar />
         {children}
       </main>
     </>
