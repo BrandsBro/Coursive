c47914fa624d55a637d0fc4b54266bf4001021bc
Mabu-1 <umahtab65@gmail.com>
2026-07-22 20:27:44 +0600

    Disable auto PageView on pixel init - MetaPixelTracker handles it

--- app/layout.jsx
+++ app/layout.jsx
@@ -56,18 +56,11 @@ export default async function RootLayout({ children }) {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1707573550631351');
+              fbq('init', '1707573550631351', {}, {autoConfig: false});
             `,
           }}
         />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1707573550631351&ev=PageView&noscript=1"
-          />
-        </noscript>
+
 
         {/* Microsoft Clarity */}
         <script
